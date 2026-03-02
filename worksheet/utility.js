7f3a88b2825c08e8abf519d1f888cd22b410f8f0
w1ll0w <vleonardo2006@gmail.com>
2026-03-02 21:57:59 +0000

    Question Attempts!
    6/6

--- worksheet/utility.js
+++ worksheet/utility.js
@@ -11,6 +11,30 @@ export function subtract(a, b) {
  * "export" to the function definition (as seen in the
  * subtract example above)
  */
-function add(a, b) {
+export function add(a, b) {
   return a + b;
 }
+
+// export function compare(arr1, arr2) {
+//   return JSON.stringify(arr1) === JSON.stringify(arr2);
+// }
+
+export function compare(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  return arr1.every((val, index) => val === arr2[index]);
+}
+
+export function largest(arr) {
+  return Math.max(...arr);
+}
+
+export function zeroest(arr) {
+  return arr.reduce((a, b) => {
+    if (Math.abs(a) === Math.abs(b)) {
+      return Math.max(a, b);
+    }
+    return Math.abs(a) < Math.abs(b) ? a : b;
+  });
+}
+
+
